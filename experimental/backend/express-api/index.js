d3b42e53fae6d64d1eec30b3121dde4b55c90def
roshan1988Sequeira <roshan016@gmail.com>
2020-11-04 12:44:58 +0530

    Delete,post,put with console response
    Added delete, put,post request with console response

--- experimental/backend/express-api/index.js
+++ experimental/backend/express-api/index.js
@@ -10,7 +10,7 @@ app.use(_bodyParsepack.json());
 //cross domain enable
 app.use(function (req,res,next){
 	res.header("Access-Control-Allow-Origin","*");
-res.header("Access-Control-Allow-Mathods", "GET,HEAD,OPTIONS,POST,PUT");
+res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept,Authorization");
 next();
 }
@@ -35,6 +35,28 @@ app.get('/', (req, res) => {
     res.send({
         data: result
     });
+});
+
+//post
+app.post('/api/create',(req,res)=>{
+var query ="insert into user (name) values (req.body.Name)";
+//QueryToExecuteInDatabase(_res,query);
+res.send('Post request to home example for insert');
+});
+
+//PUT
+app.put('/api/user',(req,res)=>{
+var query ="update usertable set name='"+req.body.Name+"'where id="+req.body.s_id;
+//QueryToExecuteInDatabase(_res,query);
+//res.send(res.json(req.body));
+res.send('Put Request working for update');
+});
+
+//Delete
+app.delete('/api/user/:id',(req,res)=>{
+var query ="Delete from user where id="+req.params.id;
+//QueryToExecuteInDatabase(_res,query);
+res.send('Delete working');
 });
 
 
