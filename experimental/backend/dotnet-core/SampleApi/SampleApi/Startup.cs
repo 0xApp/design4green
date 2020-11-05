ec7f9bbc2d3d672244dc60aeef74683d766e5ea3
Parimal Raj <ingenious.raj@gmail.com>
2020-11-05 14:33:04 +0530

    Removed usings

--- experimental/backend/dotnet-core/SampleApi/SampleApi/Startup.cs
+++ experimental/backend/dotnet-core/SampleApi/SampleApi/Startup.cs
@@ -1,17 +1,10 @@
-using System;
-using System.Collections.Generic;
 using System.IO.Compression;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.ResponseCompression;
-using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using SampleApi.Service;
 
 namespace SampleApi
