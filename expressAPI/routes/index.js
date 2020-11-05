de2859b0d270c48f68782f22e8cb83d04753c598
Sumit <itasumit@gmail.com>
2020-11-05 22:05:02 +0530

    filter with stats

--- expressAPI/routes/index.js
+++ expressAPI/routes/index.js
@@ -8,6 +8,25 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
+router.get("/master", async (req, res) => {
+  try {
+    const commune_master = await db.Commune_Master.findAll();
+    const department_master = await db.Department_Master.findAll();
+    const interCommune_master = await db.Intercommunalities_Master.findAll();
+    const region_master = await db.Region_Master.findAll();
+
+    return res.send({
+      Commune_Master: commune_master,
+      Department_Master: department_master,
+      interCommune_master: interCommune_master,
+      region_master: region_master,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.send(err);
+  }
+});
+
 router.get("/datas", (req, res) => {
   return db.Fragility_Score.findAll()
     .then((contacts) => res.send(contacts))
@@ -67,7 +86,11 @@ router.post("/datas", async (req, res) => {
     });
 
     if (foundRegion) {
-      where["LibRegion"] = foundRegion.Id;
+      if (region.exclude) {
+        where["LibRegion"] = { [Op.ne]: foundRegion.Id };
+      } else {
+        where["LibRegion"] = foundRegion.Id;
+      }
     }
   }
 
@@ -76,7 +99,11 @@ router.post("/datas", async (req, res) => {
       where: { Name: department.data },
     });
     if (foundDepartment) {
-      where["LibDepartment"] = foundDepartment.Id;
+      if (department.exclude) {
+        where["LibDepartment"] = { [Op.ne]: foundDepartment.Id };
+      } else {
+        where["LibDepartment"] = foundDepartment.Id;
+      }
     }
   }
 
@@ -85,7 +112,11 @@ router.post("/datas", async (req, res) => {
       where: { Name: intercommnality.data },
     });
     if (foundIntercommnality) {
-      where["LibInterCommunality"] = foundIntercommnality.Id;
+      if (intercommnality.exclude) {
+        where["LibInterCommunality"] = { [Op.ne]: foundIntercommnality.Id };
+      } else {
+        where["LibInterCommunality"] = foundIntercommnality.Id;
+      }
     }
   }
 
@@ -93,8 +124,13 @@ router.post("/datas", async (req, res) => {
     foundCommune = await db.Commune_Master.findOne({
       where: { Name: commune.data },
     });
+
     if (foundCommune) {
-      where["LibCommune"] = foundCommune.Id;
+      if (commune.exclude) {
+        where["LibCommune"] = { [Op.ne]: foundCommune.Id };
+      } else {
+        where["LibCommune"] = foundCommune.Id;
+      }
     }
   }
 
@@ -113,64 +149,76 @@ router.post("/datas", async (req, res) => {
     where["ScoreGlobalRegion"] = { [Op.lte]: scoreMaxValue };
   }
 
+  if (calcul) {
+    where["DonneesInfraCommunal"] = calcul ? 1 : 0;
+  }
+
+  let rankColumn;
   let columns = [];
 
-  columns.push("NomCom");
-  columns.push("CodeIris");
-  columns.push("NomIris");
-  columns.push("LibRegion");
-  columns.push("LibDepartment");
-  columns.push("LibInterCommunality");
-  columns.push("LibCommune");
-  columns.push("DonneesInfraCommunal");
-  columns.push("PopulationScore");
+  columns.push(["NomCom", "nomCom"]);
+  columns.push(["CodeIris", "codeIris"]);
+  columns.push(["NomIris", "nomIris"]);
+  columns.push(["LibRegion", "libRegion"]);
+  columns.push(["LibDepartment", "libDepartment"]);
+  columns.push(["LibInterCommunality", "libInterCommunality"]);
+  columns.push(["LibCommune", "libCommune"]);
+  columns.push(["DonneesInfraCommunal", "donneesInfraCommunal"]);
+  columns.push(["PopulationScore", "populationScore"]);
 
-  if (referencePoint === "REGION" || referencePoint === "ALL") {
-    columns.push(["ScoreGlobalRegion", "ScoreGlobale"]);
+  if (
+    referencePoint === "REGION" ||
+    referencePoint === "ALL" ||
+    referencePoint === ""
+  ) {
+    rankColumn = "ScoreGlobalRegion";
+    columns.push(["ScoreGlobalRegion", "scoreGlobal"]);
     columns.push([
       "AccessAuxInterfaceNumberRegion",
-      "AccessAuxInterfaceNumber",
+      "accessAuxInterfaceNumber",
     ]);
-    columns.push(["AccessInformationRegion", "AccessInformation"]);
-    columns.push(["GlobalAccessRegion", "GlobalAccess"]);
-    columns.push(["GlobalCompetenceRegion", "GlobalCompetence"]);
+    columns.push(["AccessInformationRegion", "accessInformation"]);
+    columns.push(["GlobalAccessRegion", "globalAccess"]);
+    columns.push(["GlobalCompetenceRegion", "globalCompetence"]);
     columns.push([
       "CompetenceAdministrativeRegion",
-      "CompetenceAdministrative",
+      "competenceAdministrative",
     ]);
-    columns.push(["CompetenceSolarisRegion", "CompetenceSolaris"]);
+    columns.push(["CompetenceSolarisRegion", "competenceSolaris"]);
   }
 
   if (referencePoint === "INTERCOMMUNALITE") {
-    columns.push(["ScoreGlobalInterCommunal", "ScoreGlobale"]);
+    rankColumn = "ScoreGlobalInterCommunal";
+    columns.push(["ScoreGlobalInterCommunal", "scoreGlobal"]);
     columns.push([
       "AccessAuxInterfaceNumberCommunal",
-      "AccessAuxInterfaceNumber",
+      "accessAuxInterfaceNumber",
     ]);
-    columns.push(["AccessInformationCommunal", "AccessInformation"]);
-    columns.push(["GlobalAccessCommunal", "GlobalAccess"]);
-    columns.push(["GlobalCompetenceCommunal", "GlobalCompetence"]);
+    columns.push(["AccessInformationCommunal", "accessInformation"]);
+    columns.push(["GlobalAccessCommunal", "globalAccess"]);
+    columns.push(["GlobalCompetenceCommunal", "globalCompetence"]);
     columns.push([
       "CompetenceAdministrativeCommunal",
-      "CompetenceAdministrative",
+      "competenceAdministrative",
     ]);
-    columns.push(["CompetenceSolarisCommunal", "CompetenceSolaris"]);
+    columns.push(["CompetenceSolarisCommunal", "competenceSolaris"]);
   }
 
   if (referencePoint === "DEPARTMENT") {
-    columns.push(["ScoreGlobalDepartment", "ScoreGlobale"]);
+    rankColumn = "ScoreGlobalDepartment";
+    columns.push(["ScoreGlobalDepartment", "scoreGlobal"]);
     columns.push([
       "AccessAuxInterfaceNumberDepartment",
-      "AccessAuxInterfaceNumber",
+      "accessAuxInterfaceNumber",
     ]);
-    columns.push(["AccessInformationDepartment", "AccessInformation"]);
-    columns.push(["GlobalAccessDepartment", "GlobalAccess"]);
-    columns.push(["GlobalCompetenceDepartment", "GlobalCompetence"]);
+    columns.push(["AccessInformationDepartment", "accessInformation"]);
+    columns.push(["GlobalAccessDepartment", "globalAccess"]);
+    columns.push(["GlobalCompetenceDepartment", "globalCompetence"]);
     columns.push([
       "CompetenceAdministrativeDepartment",
-      "CompetenceAdministrative",
+      "competenceAdministrative",
     ]);
-    columns.push(["CompetenceSolarisDepartment", "CompetenceSolaris"]);
+    columns.push(["CompetenceSolarisDepartment", "competenceSolaris"]);
   }
 
   data = await db.Fragility_Score.findAll({
@@ -180,19 +228,30 @@ router.post("/datas", async (req, res) => {
     attributes: [
       ...columns,
       [
-        Sequelize.literal("(RANK() OVER (ORDER BY ScoreGlobalRegion ASC))"),
-        "rank",
+        Sequelize.literal(`(RANK() OVER (ORDER BY ${rankColumn} DESC))`),
+        "scoreRank",
       ],
     ],
   });
 
-  return res.send(data);
-  // return db.Fragility_Score.findAll()
-  //   .then((contacts) => res.send(contacts))
-  //   .catch((err) => {
-  //     console.log("There was an error querying contacts", JSON.stringify(err));
-  //     return res.send(err);
-  //   });
+  const stats = await db.Fragility_Score.findAll({
+    where: where,
+    attributes: [
+      [Sequelize.fn("count", Sequelize.col("id")), "count"],
+      [Sequelize.fn("min", Sequelize.col(rankColumn)), "min"],
+      [Sequelize.fn("max", Sequelize.col(rankColumn)), "max"],
+    ],
+    raw: true,
+  });
+
+  let response = {
+    scores: data,
+    totalRecords: stats[0].count,
+    minScore: stats[0].min,
+    maxScore: stats[0].max,
+  };
+
+  return res.send(response);
 });
 
 module.exports = router;
