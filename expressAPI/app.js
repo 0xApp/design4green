e9b7ac2a2bd039e8b0690fdbd13b0470df55503e
Sumit <itasumit@gmail.com>
2020-11-06 08:41:50 +0530

    remove packages

--- expressAPI/app.js
+++ expressAPI/app.js
@@ -1,23 +1,21 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
 var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 
 var app = express();
 app.use(cors());
+app.disable("x-powered-by");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
